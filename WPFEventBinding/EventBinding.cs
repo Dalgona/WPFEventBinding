dffc54c74b5e9aa06b2626437828a35e4e2c9997
Eunbin Jeong <dmsqls7353@gmail.com>
2018-11-07 19:05:41 +0900

    Add EventBinding, EventBindingCollection classes

---
+++ WPFEventBinding/EventBinding.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFEventBinding
+{
+    public class EventBindingCollection : List<EventBinding> { }
+
+    public class EventBinding : DependencyObject
+    {
+        public static readonly DependencyProperty EventNameProperty =
+            DependencyProperty.Register(nameof(EventName), typeof(string), typeof(EventBinding));
+
+        public static readonly DependencyProperty CommandProperty =
+            DependencyProperty.Register(nameof(Command), typeof(ICommand), typeof(EventBinding));
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register(nameof(CommandParameter), typeof(object), typeof(EventBinding));
+
+        public string EventName
+        {
+            get => (string)GetValue(EventNameProperty);
+            set => SetValue(EventNameProperty, value);
+        }
+
+        public ICommand Command
+        {
+            get => (ICommand)GetValue(CommandProperty);
+            set => SetValue(CommandProperty, value);
+        }
+
+        public object CommandParameter
+        {
+            get => GetValue(CommandParameterProperty);
+            set => SetValue(CommandParameterProperty, value);
+        }
+    }
+}
