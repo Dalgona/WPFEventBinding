46308f10bf1db6b74ada383b0112b6f994d189a2
Eunbin Jeong <dmsqls7353@gmail.com>
2018-11-07 22:34:21 +0900

    Add RelayCommand class

---
+++ WPFEventBinding/RelayCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WPFEventBinding
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter) => _execute(parameter);
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
